34fd0c39eff48188d82bcbef12c527f97b3f9ecc
Peter Örneholm <peter@orneholm.com>
2019-10-24 09:41:51 +0200

    Wite description

--- src/Orneholm.BirdOrNot.SimpleSample/Program.cs
+++ src/Orneholm.BirdOrNot.SimpleSample/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Newtonsoft.Json;
 
 namespace Orneholm.BirdOrNot.SimpleSample
 {
@@ -13,26 +14,42 @@ namespace Orneholm.BirdOrNot.SimpleSample
         {
             Console.WriteLine("BirdOrNot");
             Console.WriteLine("-----------------");
-            Console.WriteLine("Url: ");
+            Console.WriteLine();
 
-            var url = Console.ReadLine();
+            var azureComputerVisionSubscriptionKey = Environment.GetEnvironmentVariable("AzureComputerVisionSubscriptionKey");
+            var azureComputerVisionEndpoint = Environment.GetEnvironmentVariable("AzureComputerVisionEndpoint");
 
-            var AzureComputerVisionSubscriptionKey = Environment.GetEnvironmentVariable("AzureComputerVisionSubscriptionKey");
-            var AzureComputerVisionEndpoint = Environment.GetEnvironmentVariable("AzureComputerVisionEndpoint");
-
-            var computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(AzureComputerVisionSubscriptionKey))
+            var computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(azureComputerVisionSubscriptionKey))
             {
-                Endpoint = AzureComputerVisionEndpoint
+                Endpoint = azureComputerVisionEndpoint
             };
-            
-            var imageAnalysis = await computerVisionClient.AnalyzeImageAsync(url, new List<VisualFeatureTypes>
+
+            while (true)
             {
-                VisualFeatureTypes.Objects
-            });
+                Console.WriteLine("Url: ");
+                var url = Console.ReadLine();
+                var imageAnalysis = await computerVisionClient.AnalyzeImageAsync(url, new List<VisualFeatureTypes>
+                {
+                    VisualFeatureTypes.Objects,
+                    VisualFeatureTypes.Description
+                });
 
-            var isBird = imageAnalysis.Objects.Any(x => x.ObjectProperty.Equals("bird") || CheckForBirdRecursive(x.Parent));
+                var isBird = imageAnalysis.Objects.Any(x => x.ObjectProperty.Equals("bird") || CheckForBirdRecursive(x.Parent));
 
-            Console.WriteLine(isBird ? "It is a bird." : "It is not a bird.");
+                Console.WriteLine(isBird ? "It is a bird!" : "It is not a bird.");
+                Console.WriteLine("Description: " + imageAnalysis.Description.Captions.FirstOrDefault()?.Text);
+
+                Console.WriteLine();
+
+                var json = JsonConvert.SerializeObject(imageAnalysis, Formatting.Indented);
+
+                Console.WriteLine("Full result:");
+                Console.WriteLine("------------------------");
+                Console.WriteLine(json);
+                Console.WriteLine("------------------------");
+
+                Console.WriteLine();
+            }
         }
 
         private static bool CheckForBirdRecursive(ObjectHierarchy detectedObject)
